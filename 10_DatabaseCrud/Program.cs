33988ceb77bbabde28aaeecc750f04650906f756
Yasin Cüre <148881966+yasindenizcure@users.noreply.github.com>
2024-12-10 04:02:07 +0300

    Add files via upload

---
+++ 10_DatabaseCrud/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_DatabaseCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            //Crud --> Create-Read-Update-Delete
+            Console.WriteLine("***** Menü Sipariş İşlem Paneli *****");
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------");
+            #region Kategori Ekleme İşlemi
+            //Console.Write("Eklemek istediğiniz kategori adı: ");
+            //string categoryName = Console.ReadLine();
+
+            //SqlConnection connection = new SqlConnection("Data Source=DESKTOP-8TRT9BV\\MSSQLSERVER01;initial catalog=DbEgitimKampi;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("insert into TblCategory (CategoryName) values (@p1)", connection);
+            //command.Parameters.AddWithValue("@p1",categoryName);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+
+            //Console.Write("Kategori başarıyla eklendi.");
+            #endregion
+
+            #region Ürün Ekleme İşlemi
+
+            //string productName;
+            //decimal productPrice;
+            //bool productStatus;
+
+            //Console.Write("Ürün Adı: ");
+            //productName = Console.ReadLine();
+            //Console.Write("Ürün Fiyatı: ");
+            //productPrice = decimal.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data Source=DESKTOP-8TRT9BV\\MSSQLSERVER01;initial catalog=DbEgitimKampi;integrated security=true");
+            //connection.Open();
+
+            //SqlCommand command = new SqlCommand("insert into TblProduct (ProductName,ProductPrice,ProductStatus) values (@productName,@productPrice,@productStatus)",connection);
+            //command.Parameters.AddWithValue("@productName",productName);
+            //command.Parameters.AddWithValue("@productPrice",productPrice);
+            //command.Parameters.AddWithValue("@productStatus",true);
+            //command.ExecuteNonQuery();
+
+            //connection.Close();
+
+            //Console.Write("ürün eklemesi başarılı.");
+
+            #endregion
+
+            #region Ürün Listeleme İşlemi (Select)
+
+            //SqlConnection connection = new SqlConnection("Data Source=DESKTOP-8TRT9BV\\MSSQLSERVER01;initial catalog=DbEgitimKampi;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Select * From TblProduct", connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+            //DataTable dataTable = new DataTable();
+            //adapter.Fill(dataTable);
+            //foreach (DataRow row in dataTable.Rows) 
+            //{
+            //    foreach (var item in row.ItemArray) 
+            //    {
+            //        Console.Write(item.ToString()+ " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //connection.Close();
+
+            #endregion
+
+            #region Ürün Silme İşlemi (Delete)
+
+            //Console.Write("Silinecek Ürün ID'sini giriniz: ");
+            //int productId = int.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data Source=DESKTOP-8TRT9BV\\MSSQLSERVER01;initial catalog=DbEgitimKampi;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Delete From TblProduct Where ProductId=@productId",connection);
+            //command.Parameters.AddWithValue("@productId", productId);
+            //command.ExecuteNonQuery();
+
+            //connection.Close();
+
+            //Console.WriteLine("Silme işlemi gerçekleştirildi.");
+
+            #endregion
+
+            #region Ürün Güncelleme İşlemi (Update)
+
+            //Console.Write("Güncellenecek Ürün ID'sini giriniz: ");
+            //int productId = int.Parse(Console.ReadLine());
+
+            //Console.Write("Güncellenecek Ürün Adını giriniz: ");
+            //string productName = Console.ReadLine();
+
+            //Console.Write("Güncellenecek Ürün Fiyatını giriniz: ");
+            //decimal productPrice = decimal.Parse(Console.ReadLine());
+
+
+            //SqlConnection connection = new SqlConnection("Data Source=DESKTOP-8TRT9BV\\MSSQLSERVER01;initial catalog=DbEgitimKampi;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Update TblProduct Set ProductName=@productName,ProductPrice=@productPrice where ProductId=@productId",connection);
+            //command.Parameters.AddWithValue("@productName",productName);
+            //command.Parameters.AddWithValue("@productPrice",productPrice);
+            //command.Parameters.AddWithValue("@productId",productId);
+            //command.ExecuteNonQuery();
+
+            //connection.Close();
+
+            //Console.WriteLine("Güncelleme Başarılı bir şekilde gerçekleştirildi.");
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
