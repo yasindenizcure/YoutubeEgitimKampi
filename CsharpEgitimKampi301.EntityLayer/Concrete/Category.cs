10fb8f23291bbf0ee2230519e2bae322e2eec09e
Yasin Cüre <148881966+yasindenizcure@users.noreply.github.com>
2024-12-11 03:57:35 +0300

    Add files via upload

---
+++ CsharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+
+    //int x; ---> Field (Class'ın içinde)
+    //public int y {get; set;} ---> Property
+    //void test() {int z;} ---> Variable (Metot içinde tanımlanıyorsa)
+}
