69435801def32a07dde0e1d5a35cc332362965f5
Yasin Cüre <148881966+yasindenizcure@users.noreply.github.com>
2024-12-13 16:55:36 +0300

    Add files via upload

---
+++ CsharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+
+    //int x; ---> Field (Class'ın içinde)
+    //public int y {get; set;} ---> Property
+    //void test() {int z;} ---> Variable (Metot içinde tanımlanıyorsa)
+}
---
+++ CsharpEgitimKampi301.EntityLayer/Concrete/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
