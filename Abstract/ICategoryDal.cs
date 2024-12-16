7e2c060e960a7a90aadb1f229ba4cf17e165672c
Yasin Cüre <148881966+yasindenizcure@users.noreply.github.com>
2024-12-16 03:25:28 +0300

    Add files via upload

---
+++ Abstract/ICategoryDal.cs
@@ -0,0 +1,13 @@
+﻿using CsharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpEgitimKampi301.DataAccessLayer.Abstract
+{
+    public interface ICategoryDal:IGenericDal<Category>
+    {
+    }
+}
