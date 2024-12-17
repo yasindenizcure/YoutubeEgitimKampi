c58affc601f248fdb22ef7bb21142d59bd8b2f4f
Yasin Cüre <148881966+yasindenizcure@users.noreply.github.com>
2024-12-17 16:01:46 +0300

    Add files via upload

--- CsharpEgitimKampi301.EFProject/Form1.cs
+++ CsharpEgitimKampi301.EFProject/Form1.cs
@@ -16,5 +16,52 @@ namespace CsharpEgitimKampi301.EFProject
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        EgitimKampiEfTravelDbEntities db = new EgitimKampiEfTravelDbEntities();
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = db.Guide.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Guide guide = new Guide();
+            guide.GuideName = txtName.Text;
+            guide.GuideSurname = txtSurname.Text;
+            db.Guide.Add(guide);
+            db.SaveChanges();
+            MessageBox.Show("Rehber başarıyla eklendi.");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id =int.Parse(txtId.Text);
+            var removeValue =db.Guide.Find(id);
+            db.Guide.Remove(removeValue);
+            db.SaveChanges();
+            MessageBox.Show("Rehber başarıyla silindi.");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var updateValue = db.Guide.Find(id);
+            updateValue.GuideName = txtName.Text;
+            updateValue.GuideSurname= txtSurname.Text;
+            db.SaveChanges();
+            MessageBox.Show("Rehber başarıyla güncellendi.", "Uyarı", MessageBoxButtons.OK,MessageBoxIcon.Warning);
+        }
+
+        private void btnGetByID_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var values = db.Guide.Where(x => x.GuideId == id).ToList();
+            dataGridView1.DataSource = values;
+        }
     }
 }
