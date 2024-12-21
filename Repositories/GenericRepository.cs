50b14974687af2f33c0c0c0c662b076fdc277b27
Yasin Cüre <148881966+yasindenizcure@users.noreply.github.com>
2024-12-21 18:01:49 +0300

    Add files via upload

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using CsharpEgitimKampi301.DataAccessLayer.Abstract;
+using CsharpEgitimKampi301.DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpEgitimKampi301.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        KampContext context = new KampContext();
+        private readonly DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            _object=context.Set<T>();
+        }
+        public void Delete(T entity)
+        {
+            var deletedEntity = context.Entry(entity);
+            deletedEntity.State =EntityState.Deleted;
+            context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _object.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _object.Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            var updatedEntity = context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+}
