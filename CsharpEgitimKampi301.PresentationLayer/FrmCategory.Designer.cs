6ec19c6698bc2db98e8edec6d83e662b2ff7c9f4
Yasin Cüre <148881966+yasindenizcure@users.noreply.github.com>
2024-12-25 16:27:22 +0300

    Add files via upload

--- CsharpEgitimKampi301.PresentationLayer/FrmCategory.Designer.cs
+++ CsharpEgitimKampi301.PresentationLayer/FrmCategory.Designer.cs
@@ -68,6 +68,7 @@
             this.btnList.TabIndex = 2;
             this.btnList.Text = "Listele";
             this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
             // 
             // dataGridView1
             // 
@@ -134,6 +135,7 @@
             this.btnAdd.TabIndex = 9;
             this.btnAdd.Text = "Ekle";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnDelete
             // 
@@ -143,6 +145,7 @@
             this.btnDelete.TabIndex = 10;
             this.btnDelete.Text = "Sil";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnUpdate
             // 
@@ -152,6 +155,7 @@
             this.btnUpdate.TabIndex = 11;
             this.btnUpdate.Text = "Güncelle";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnGetByID
             // 
@@ -161,6 +165,7 @@
             this.btnGetByID.TabIndex = 12;
             this.btnGetByID.Text = "ID\'ye göre getir";
             this.btnGetByID.UseVisualStyleBackColor = true;
+            this.btnGetByID.Click += new System.EventHandler(this.btnGetByID_Click);
             // 
             // FrmCategory
             // 
--- CsharpEgitimKampi301.PresentationLayer/FrmCategory.cs
+++ CsharpEgitimKampi301.PresentationLayer/FrmCategory.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CsharpEgitimKampi301.BusinessLayer.Abstract;
+using CsharpEgitimKampi301.BusinessLayer.Concrete;
+using CsharpEgitimKampi301.DataAccessLayer.EntityFramework;
+using CsharpEgitimKampi301.EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +16,52 @@ namespace CsharpEgitimKampi301.PresentationLayer
 {
     public partial class FrmCategory : Form
     {
+        private readonly ICategoryService _categoryService;
+
         public FrmCategory()
         {
+            _categoryService = new CategoryManager(new EfCategoryDal());
             InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var categoryValues = _categoryService.TGetAll();
+            dataGridView1.DataSource = categoryValues;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Category category = new Category();
+            category.CategoryName = txtCategoryName.Text;
+            category.CategoryStatus = true;
+            _categoryService.TInsert(category);
+            MessageBox.Show("Ekleme işlemi başarılı.");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtCategoryId.Text);
+            var deletedValues = _categoryService.TGetById(id);
+            _categoryService.TDelete(deletedValues);
+            MessageBox.Show("Silme işlemi başarılı.");
+        }
+
+        private void btnGetByID_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtCategoryId.Text);
+            var values= _categoryService.TGetById(id);
+            dataGridView1 .DataSource = values;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int updatedId = int.Parse(txtCategoryId.Text);
+            var updatedValue = _categoryService.TGetById(updatedId);
+            updatedValue.CategoryName = txtCategoryName.Text;
+            updatedValue.CategoryStatus = true;
+            _categoryService.TUpdate(updatedValue);
+            MessageBox.Show("Güncelleme işlemi başarılı.");
         }
     }
 }
