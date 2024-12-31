ccab86ea73c3e9e9e05d030b0cfcd5e1fb84021e
Yasin Cüre <148881966+yasindenizcure@users.noreply.github.com>
2024-12-31 15:07:44 +0300

    Add files via upload

---
+++ CsharpEgitimKampi301.PresentationLayer/FrmProduct.cs
@@ -0,0 +1,50 @@
+﻿using CsharpEgitimKampi301.BusinessLayer.Abstract;
+using CsharpEgitimKampi301.BusinessLayer.Concrete;
+using CsharpEgitimKampi301.DataAccessLayer.Abstract;
+using CsharpEgitimKampi301.DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsharpEgitimKampi301.PresentationLayer
+{
+    public partial class FrmProduct : Form
+    {
+        private readonly IProductService _productService;
+        public FrmProduct()
+        {
+            InitializeComponent();
+            _productService = new ProductManager(new EfProductDal());
+        }
+
+
+       
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = _productService.TGetAll();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnList2_Click(object sender, EventArgs e)
+        {
+            var values = _productService.TGetProductsWithCategory();
+            dataGridView1 .DataSource = values;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
