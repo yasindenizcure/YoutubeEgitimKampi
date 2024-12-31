322e784927666f3a409989e758c3cf8789eceb5e
Yasin Cüre <148881966+yasindenizcure@users.noreply.github.com>
2024-12-31 15:07:05 +0300

    Add files via upload

---
+++ CsharpEgitimKampi301.DataAccessLayer/EntityFramework/EfOrderDal.cs
@@ -0,0 +1,15 @@
+﻿using CsharpEgitimKampi301.DataAccessLayer.Abstract;
+using CsharpEgitimKampi301.DataAccessLayer.Repositories;
+using CsharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpEgitimKampi301.DataAccessLayer.EntityFramework
+{
+    public class EfOrderDal: GenericRepository<Order>,IOrderDal
+    {
+    }
+}
---
+++ CsharpEgitimKampi301.DataAccessLayer/EntityFramework/EfProductDal.cs
@@ -0,0 +1,32 @@
+﻿using CsharpEgitimKampi301.DataAccessLayer.Abstract;
+using CsharpEgitimKampi301.DataAccessLayer.Context;
+using CsharpEgitimKampi301.DataAccessLayer.Repositories;
+using CsharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpEgitimKampi301.DataAccessLayer.EntityFramework
+{
+    public class EfProductDal : GenericRepository<Product>, IProductDal
+    {
+        public List<Object> GetProductsWithCategory()
+        {
+            var context = new KampContext();
+            var values = context.Products
+                .Select(x => new 
+                {
+                    ProductId = x.ProductId,
+                    ProductName = x.ProductName,
+                    ProductStock = x.ProductStock,
+                    ProductPrice = x.ProductPrice,
+                    ProductDescription = x.ProductDescription,
+                    CategoryName = x.Category.CategoryName,
+                }).ToList();
+
+            return values.Cast<object>().ToList();
+        }
+    }
+}
